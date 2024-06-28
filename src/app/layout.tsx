69b30545dbae2f31547e36e66c43afe6cbe0aab0
viqqib <faqihzaky43@gmail.com>
2024-06-28 17:33:00 +0800

    adding web icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 import { StoreProvider } from '@/redux/StoreProvider';
 import '@fontsource/dm-sans/latin.css';
 import '@fontsource/dm-serif-display/latin.css';
 import 'tailwindcss/tailwind.css';
 import '@fontsource/nunito-sans/latin.css'; 
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Stickoala - Sticker & Pin Store",
+  title: "Stickoala - Sticker & Pin",
   description: "Sticker Store",
 };
 
@@ -22,9 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="/stickerprint.svg" />
       </head>
-      <body className={`${inter.className} bg-greenst`}>
+      <body className="bg-greenst">
         <StoreProvider>
           {children}
         </StoreProvider>
